bed76700401a7114c76d9b7bdfa0ca1f822f2a2f
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2019-11-01 14:39:06 -0300

    desfazer acao com snackbar e reordenar itens

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,21 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b){
+        if(a['ok'] && !b['ok']) return 1;
+        else if(!a['ok'] && b['ok']) return -1;
+        else return 0;
+      });
+
+      _saveData();
+
+    });
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,10 +93,13 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 10.0),
-              itemCount: _toDoList.length,
-              itemBuilder: buildItem,
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: buildItem,
+              ),
             ),
           ),
         ],
@@ -89,7 +107,7 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Widget buildItem(context, index){
+  Widget buildItem(BuildContext context, int index){
    return Dismissible(
      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
      background: Container(
@@ -115,6 +133,31 @@ class _HomeState extends State<Home> {
     });
     },
     ),
+     onDismissed: (direction){
+       setState(() {
+         _lastRemoved = Map.from(_toDoList[index]);
+         _lastRemovedPos = index;
+         _toDoList.removeAt(index);
+
+         _saveData();
+
+         final snack = SnackBar(
+           content: Text('Tarefa \"${_lastRemoved['title']}\" removida'),
+           action: SnackBarAction(label: 'Desfazer',
+             onPressed: (){
+              setState(() {
+                _toDoList..insert(_lastRemovedPos, _lastRemoved);
+                _saveData();
+              });
+             },),
+           duration: Duration(seconds: 2),
+         );
+
+         Scaffold.of(context).removeCurrentSnackBar(); //remove a snackbar atual antes de mostrar a nova
+         Scaffold.of(context).showSnackBar(snack);
+
+       });
+     },
    );
   }
 
